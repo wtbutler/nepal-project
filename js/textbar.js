a9fff4680128595441276ef858db907cf5c27ef9
wtbutler <wtbutler2@gmail.com>
2021-03-18 23:30:27 -0500

    Misc fixes

--- js/textbar.js
+++ js/textbar.js
@@ -22,7 +22,7 @@ function closeNav() {
 
 function changeText(filename) {
     console.log("Opening: " + filename + ".txt");
-    var pathname = "https://wtbutler.github.io/nepal-project/info/"+filename+".html";
+    var pathname = "https://wtbutler.github.io/tibet-project/info/"+filename+".html";
     fetch(pathname)
         .then(response => response.text())
         .then(text => document.getElementById("desc").innerHTML = text);
